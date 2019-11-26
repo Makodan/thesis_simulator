bb45f4a2bdb27bccd26375a9bfcc511883decb29
makoda <mako.daniel@outlook.com>
2019-11-26 19:06:01 +0100

    refined frequency tuning, graph plotting

--- des_dens.py
+++ des_dens.py
@@ -42,9 +42,38 @@ eai_fmin = 0.25
 
 
 class Sensor():
-    def __init__(self, id, frequency, ai_mode):
+    def __init__(self, id, ai_mode):
+        # AI mode dependant variables
+        if ai_mode == 0:
+            self.delay = cai_delay
+            # self.max_freq = cai_freq
+            self.packet_size = cai_packet
+            self.cps = cai_cps
+            self.accuracy = 0.85
+            self.completeness = 0.5
+            self.fmax = cai_fmax
+            self.fmin = cai_fmin
+        if ai_mode == 1:
+            self.delay = noai_delay
+            # self.max_freq = frequency
+            self.packet_size = noai_packet
+            self.cps = noai_cps
+            self.accuracy = 1
+            self.completeness = 0
+            self.fmax = noai_fmax
+            self.fmin = noai_fmin
+        if ai_mode == 2:
+            self.delay = eai_delay
+            # self.max_freq = eai_freq
+            self.packet_size = eai_packet
+            self.cps = eai_cps
+            self.accuracy = 0.6
+            self.completeness = 0.5
+            self.fmax = eai_fmax
+            self.fmin = eai_fmin
+
         self.id = id
-        self.frequency = frequency
+        self.frequency = self.fmin
         self.ai_mode = ai_mode  # 0 - centralized AI, 1 - no AI, 2 - edge AI
         self.neighbours = list()
         self.event = False
@@ -55,40 +84,12 @@ class Sensor():
         self.samples = 0
 
         # Per sec variables
-        self.actual_frequency = frequency
-        self.base_frequency = frequency
+        self.actual_frequency = self.frequency
         self.actual_bandwidth = 0
         self.actual_power = 0
         self.actual_quality = 0
 
-        # AI mode dependant variables
-        if ai_mode == 0:
-            self.delay = cai_delay
-            self.max_freq = cai_freq
-            self.packet_size = cai_packet
-            self.cps = cai_cps
-            self.accuracy = 0.85
-            self.completeness = 0.5
-            self.fmax = cai_fmax
-            self.fmin = cai_fmin
-        if ai_mode == 1:
-            self.delay = noai_delay
-            self.max_freq = frequency
-            self.packet_size = noai_packet
-            self.cps = noai_cps
-            self.accuracy = 1
-            self.completeness = 0
-            self.fmax = noai_fmax
-            self.fmin = noai_fmin
-        if ai_mode == 2:
-            self.delay = eai_delay
-            self.max_freq = eai_freq
-            self.packet_size = eai_packet
-            self.cps = eai_cps
-            self.accuracy = 0.6
-            self.completeness = 0.5
-            self.fmax = eai_fmax
-            self.fmin = eai_fmin
+
 
     def step(self):
         # If there is new event process it, calculate with higher freqs,
@@ -97,17 +98,17 @@ class Sensor():
             # If the event's duration or the remaining duration is shorter than a timestep
             if self.event_duration <= timestep:
                 if not self.delay_handled:
-                    self.actual_frequency = self.max_freq
-                    self.samples += floor((self.event_duration - self.delay) * self.max_freq)
+                    self.actual_frequency = self.fmax
+                    self.samples += floor((self.event_duration - self.delay) * self.actual_frequency)
                     self.delay_handled = True
-                    self.samples += floor((timestep - self.event_duration) * self.frequency)
+                    self.samples += floor((timestep - self.event_duration) * self.actual_frequency)
 
                 else:
                     # Max frequency samples
-                    self.samples += floor(self.event_duration * self.max_freq)
+                    self.samples += floor(self.event_duration * self.actual_frequency)
                     # Base frequency samples
-                    self.samples += floor((timestep - self.event_duration) * self.frequency)
-                    self.actual_frequency = self.frequency
+                    self.actual_frequency = self.fmin
+                    self.samples += floor((timestep - self.event_duration) * self.actual_frequency)
 
                 self.event = False
                 self.delay_handled = False
@@ -117,19 +118,19 @@ class Sensor():
             else:
                 # The delay has NOT handled already
                 if not self.delay_handled:
-                    self.actual_frequency = self.max_freq
-                    self.samples += floor((timestep - self.delay) * self.max_freq)
+                    self.actual_frequency = self.fmax
+                    self.samples += floor((timestep - self.delay) * self.actual_frequency)
                     # self.event_duration -= self.delay
                     self.event_duration -= timestep
                     self.delay_handled = True
                 # The delay has handled already
                 else:
-                    self.samples += floor(timestep * self.max_freq)
+                    self.samples += floor(timestep * self.actual_frequency)
                     self.event_duration -= timestep
 
         # If there is no event, calculate with base freq
         else:
-            self.samples += (timestep * self.frequency)
+            self.samples += (timestep * self.actual_frequency)
 
         # Summarize energy, sent bytes based on samples
         # Calculate E, B
@@ -137,15 +138,16 @@ class Sensor():
         # Calculate actual metrics
         # Edge AI needs another method to calculate,if there is event
         self.actual_bandwidth = self.actual_frequency * self.packet_size
+        self.actual_power = self.actual_frequency * self.cps
+        self.actual_quality = ((1 - self.delay * self.actual_frequency) + self.accuracy + (
+                self.actual_frequency / self.fmax) + self.completeness) / 4
         if self.ai_mode == 2 and self.event:
             self.actual_bandwidth = self.actual_frequency * noai_packet
-        self.actual_power = self.actual_frequency * self.cps
-        self.actual_quality = ((1-self.delay*self.actual_frequency) + self.accuracy + (
-                    self.actual_frequency / self.max_freq) + self.completeness) / 4
-        print(self.actual_quality)
+            self.actual_power = self.actual_frequency * eai_cps_send
+
         self.sent_bytes += self.samples * self.packet_size
         self.consumption += self.samples * self.cps
-        print(f"ID:{self.id} samples: {self.samples} sent bytes: {self.sent_bytes}")
+        # print(f"ID:{self.id} samples: {self.samples} sent bytes: {self.sent_bytes}")
         self.samples = 0
 
     def register_event(self, duration):
@@ -166,7 +168,7 @@ class Sensor():
 
 if __name__ == '__main__':
     sensor_list = list()
-    simulation_length = 1100  # sec
+    simulation_length = 3600  # sec
 
     population_step = 1000
     population_max = 10000  # number
@@ -174,7 +176,11 @@ if __name__ == '__main__':
     event_max = 120
     ai = 2
     freq =0.25
-    for ai_v in range(1,2,1):
+    color_list = ("red", "green", "grey")
+    # ai_mode  0 - centralized AI, 1 - no AI, 2 - edge AI
+    label_list = ("CAI", "NoAI", "EAI")
+    for ai_v in range(0,3,1):
+
         consumption_list = list()
         bytes_list = list()
         delay_list = list()
@@ -184,7 +190,7 @@ if __name__ == '__main__':
         actual_quality_list = list()
         event_list = list()
     # Test with 1 sensor
-        s1 = Sensor(1, freq, ai_v)
+        s1 = Sensor(1, ai_v)
         for t in range(0, simulation_length, timestep):
             s1.step()
             actual_power_list.append(s1.actual_power)
@@ -192,26 +198,49 @@ if __name__ == '__main__':
             actual_frequency_list.append(s1.actual_frequency)
             actual_quality_list.append(s1.actual_quality)
             consumption_list.append(s1.consumption)
-            if t == 1000:
-                s1.register_event(100)
-            # s1.event_generator()
-            if t >= 1000 and t < 1100:
+            if t == 800:
+                s1.register_event(120)
+            if t == 1700:
+                s1.register_event(300)
+            if (t >= 800 and t < 920) or (1700 <= t < 2000):
                 event_list.append(1)
             else:
                 event_list.append(0)
 
-        plt.figure('Event')
-        plt.plot(event_list)
-        plt.figure('Power')
-        plt.plot(actual_power_list)
-        plt.figure('Consumption')
-        plt.plot(consumption_list)
-        plt.figure('Frequency')
-        plt.plot(actual_frequency_list)
-        plt.figure('Bandwith')
-        plt.plot(actual_bandwidth_list)
-        plt.figure('Quality')
-        plt.plot(actual_quality_list)
+        plt.figure('Graphs')
+
+        plt.subplot(611)
+        plt.ylabel('Event')
+        plt.plot(event_list, color="blue")
+        plt.subplot(612)
+        plt.ylabel('Power')
+        line = plt.plot(actual_power_list, color=color_list[ai_v], label=label_list[ai_v])
+        plt.subplot(613)
+        plt.ylabel('Consumption')
+        plt.plot(consumption_list, color=color_list[ai_v], label=label_list[ai_v])
+        plt.subplot(614)
+        plt.ylabel('Actual freq')
+        plt.plot(actual_frequency_list, color=color_list[ai_v], label=label_list[ai_v])
+        plt.subplot(615)
+        plt.ylabel('Bandwidth')
+        plt.plot(actual_bandwidth_list, color=color_list[ai_v], label=label_list[ai_v])
+        plt.subplot(616)
+        plt.ylabel('Quality')
+        plt.plot(actual_quality_list, color=color_list[ai_v], label=label_list[ai_v])
+        plt.legend(loc="upper left")
+        # plt.figure('Event')
+        # plt.plot(event_list)
+        # plt.figure('Power')
+        # line = plt.plot(actual_power_list)
+        # plt.legend((line,),(str(ai_v),))
+        # plt.figure('Consumption')
+        # plt.plot(consumption_list)
+        # plt.figure('Frequency')
+        # plt.plot(actual_frequency_list)
+        # plt.figure('Bandwith')
+        # plt.plot(actual_bandwidth_list)
+        # plt.figure('Quality')
+        # plt.plot(actual_quality_list)
     plt.show()
 
     # for population in range(1, population_max, population_step):
